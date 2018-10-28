04eb270bf31b5700674498459ed88074286b7821
Graham Shapiro <virtualgraham@gmail.com>
2018-10-28 11:19:44 +0000

    None

--- autoencoder/model_tools.py
+++ autoencoder/model_tools.py
@@ -1,4 +1,5 @@
-import numpy as npsdfsa 
+import numpy as np 
+import h5py
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from data_prep import list_training_files, list_testing_files
@@ -10,11 +11,11 @@ from data_prep import list_training_files, list_testing_files
 
 epochs = 1
 learning_rate = 0.001
-batch_size = 10
+batch_size = 3
 
 model_path = "./autoencoder/model/model.ckpt"
 
-def read_image_file(filename, _):
+def read_image_file(filename):
         
         print('filename', filename)
 
@@ -22,16 +23,18 @@ def read_image_file(filename, _):
 
         image = tf.image.decode_png(image_string, channels=3)
         image = tf.image.convert_image_dtype(image, tf.float32)
-
-        target_image = tf.image.decode_png(image_string, channels=3)
-        target_image = tf.image.convert_image_dtype(target_image, tf.float32)
         
-        return image, target_image
+        return image
 
-is_training = True
-training_files = list_training_files()[:100]
+is_training = False
+training_files = list_testing_files()
         
-dataset = tf.data.Dataset.from_tensor_slices((training_files, training_files))
+
+##########
+# ENCODER #
+##########
+
+dataset = tf.data.Dataset.from_tensor_slices((training_files))
 dataset = dataset.shuffle(len(training_files))
 dataset = dataset.map(read_image_file, num_parallel_calls=4)
 dataset = dataset.batch(batch_size)
@@ -39,14 +42,13 @@ dataset = dataset.prefetch(1)
 dataset = dataset.repeat(-1)
 
 iterator = dataset.make_one_shot_iterator()
-img_in, img_out = iterator.get_next()
+img_in = iterator.get_next()
 
-inputs_ = tf.placeholder_with_default(img_in, (None, 128, 384, 3), name='inputs')
-targets_ = tf.placeholder_with_default(img_out, (None, 128, 384, 3), name='target')
+encoderInputs = tf.placeholder_with_default(img_in, (None, 128, 384, 3), name='encoderInputs')
 
 # Now 128x384x3
 
-conv1 = tf.layers.conv2d(inputs=inputs_, filters=64, kernel_size=(5,5), padding='same', kernel_initializer=tf.glorot_normal_initializer(), activation=tf.nn.relu, name="conv1")
+conv1 = tf.layers.conv2d(inputs=encoderInputs, filters=64, kernel_size=(3,3), padding='same', kernel_initializer=tf.glorot_normal_initializer(), activation=tf.nn.relu, name="conv1")
 conv1_norm = tf.layers.batch_normalization(conv1, training=is_training, name="conv1_norm")
 print('conv1', conv1.get_shape())
 # Now 128x384x32
@@ -55,7 +57,7 @@ maxpool1 = tf.layers.max_pooling2d(conv1_norm, pool_size=(2,2), strides=(2,2), p
 print('maxpool1', maxpool1.get_shape())
 # Now 64x192x32
 
-conv2 = tf.layers.conv2d(inputs=maxpool1, filters=48, kernel_size=(5,5), padding='same', kernel_initializer=tf.glorot_normal_initializer(), activation=tf.nn.relu, name="conv2")
+conv2 = tf.layers.conv2d(inputs=maxpool1, filters=48, kernel_size=(3,3), padding='same', kernel_initializer=tf.glorot_normal_initializer(), activation=tf.nn.relu, name="conv2")
 conv2_norm = tf.layers.batch_normalization(conv2, training=is_training, name="conv2_norm")
 print('conv2', conv2.get_shape())
 # Now 64x192x32
@@ -64,7 +66,7 @@ maxpool2 = tf.layers.max_pooling2d(conv2_norm, pool_size=(2,2), strides=(2,2), p
 print('maxpool2', maxpool2.get_shape())
 # Now 32x96x32
 
-conv3 = tf.layers.conv2d(inputs=maxpool2, filters=32, kernel_size=(5,5), padding='same', kernel_initializer=tf.glorot_normal_initializer(), activation=tf.nn.relu, name="conv3")
+conv3 = tf.layers.conv2d(inputs=maxpool2, filters=32, kernel_size=(3,3), padding='same', kernel_initializer=tf.glorot_normal_initializer(), activation=tf.nn.relu, name="conv3")
 conv3_norm = tf.layers.batch_normalization(conv3, training=is_training, name="conv3_norm")
 print('conv3', conv3.get_shape())
 # Now 32x96x32
@@ -73,7 +75,7 @@ maxpool3 = tf.layers.max_pooling2d(conv3_norm, pool_size=(2,2), strides=(2,2), p
 print('maxpool3', maxpool3.get_shape())
 # Now 16x48x32
 
-conv4 = tf.layers.conv2d(inputs=maxpool3, filters=32, kernel_size=(5,5), padding='same', kernel_initializer=tf.glorot_normal_initializer(), activation=tf.nn.relu, name="conv4")
+conv4 = tf.layers.conv2d(inputs=maxpool3, filters=32, kernel_size=(3,3), padding='same', kernel_initializer=tf.glorot_normal_initializer(), activation=tf.nn.relu, name="conv4")
 conv4_norm = tf.layers.batch_normalization(conv4, training=is_training, name="conv4_norm")
 print('conv4', conv4.get_shape())
 # Now 16x48x32
@@ -82,15 +84,34 @@ encoded = tf.layers.max_pooling2d(conv4_norm, pool_size=(2,2), strides=(2,2), pa
 print('encoded', encoded.get_shape())
 # Now 8x24x32
 
+##########
+# DECODER #
+##########
 
+def read_encoded_image_file(filename):
+    data = np.load(filename.decode())
+    return data.astype(np.float32)
+
+encoded_files = ['./autoencoder/test/test0.npz.npy','./autoencoder/test/test1.npz.npy','./autoencoder/test/test2.npz.npy']
+
+decoder_dataset = tf.data.Dataset.from_tensor_slices((encoded_files))
+decoder_dataset = decoder_dataset.map(lambda item: tf.py_func(read_encoded_image_file, [item], tf.float32))
+decoder_dataset = decoder_dataset.batch(batch_size)
+decoder_dataset = decoder_dataset.prefetch(1)
+decoder_dataset = decoder_dataset.repeat(-1)
+
+decoder_iterator = decoder_dataset.make_one_shot_iterator()
+encoded_in = decoder_iterator.get_next()
+
+decoderInputs = tf.placeholder_with_default(encoded_in, (None, 8, 24, 32), name='decoderInputs')
 
 ### Decoder
-upsample1 = tf.layers.conv2d_transpose(encoded, 32, 2, strides=2, padding='same', name="upsample1")
+upsample1 = tf.layers.conv2d_transpose(decoderInputs, 32, 2, strides=2, padding='same', name="upsample1")
 upsample1_norm = tf.layers.batch_normalization(upsample1, training=is_training, name="upsample1_norm")
 print('upsample1', upsample1.get_shape())
 # Now 16x48x16
 
-conv5 = tf.layers.conv2d(inputs=upsample1_norm, filters=32, kernel_size=(5,5), padding='same', activation=tf.nn.relu, name="conv5")
+conv5 = tf.layers.conv2d(inputs=upsample1_norm, filters=32, kernel_size=(3,3), padding='same', activation=tf.nn.relu, name="conv5")
 conv5_norm = tf.layers.batch_normalization(conv5, training=is_training, name="conv5_norm")
 print('conv5', conv5.get_shape())
 # Now 16x48x16
@@ -100,7 +121,7 @@ upsample2_norm = tf.layers.batch_normalization(upsample2, training=is_training, 
 print('upsample2', upsample2.get_shape())
 # Now 32x96x16
 
-conv6 = tf.layers.conv2d(inputs=upsample2_norm, filters=32, kernel_size=(5,5), padding='same', kernel_initializer=tf.glorot_normal_initializer(), activation=tf.nn.relu, name="conv6")
+conv6 = tf.layers.conv2d(inputs=upsample2_norm, filters=32, kernel_size=(3,3), padding='same', kernel_initializer=tf.glorot_normal_initializer(), activation=tf.nn.relu, name="conv6")
 conv6_norm = tf.layers.batch_normalization(conv6, training=is_training, name="conv6_norm")
 print('conv6', conv6.get_shape())
 # Now 32x96x16
@@ -110,7 +131,7 @@ upsample3_norm = tf.layers.batch_normalization(upsample3, training=is_training, 
 print('upsample3', upsample3.get_shape())
 # Now 64x192x16
 
-conv7 = tf.layers.conv2d(inputs=upsample3_norm, filters=48, kernel_size=(5,5), padding='same', kernel_initializer=tf.glorot_normal_initializer(), activation=tf.nn.relu, name="conv7")
+conv7 = tf.layers.conv2d(inputs=upsample3_norm, filters=48, kernel_size=(3,3), padding='same', kernel_initializer=tf.glorot_normal_initializer(), activation=tf.nn.relu, name="conv7")
 conv7_norm = tf.layers.batch_normalization(conv7, training=is_training, name="conv7_norm")
 print('conv7', conv7.get_shape())
 # Now 64x192x32
@@ -120,72 +141,60 @@ upsample4_norm = tf.layers.batch_normalization(upsample4, training=is_training, 
 print('upsample4', upsample4.get_shape())
 # Now 128x384x32
 
-conv8 = tf.layers.conv2d(inputs=upsample4_norm, filters=64, kernel_size=(5,5), padding='same', kernel_initializer=tf.glorot_normal_initializer(), activation=tf.nn.relu, name="conv8")
+conv8 = tf.layers.conv2d(inputs=upsample4_norm, filters=64, kernel_size=(3,3), padding='same', kernel_initializer=tf.glorot_normal_initializer(), activation=tf.nn.relu, name="conv8")
 conv8_norm = tf.layers.batch_normalization(conv8, training=is_training, name="conv8_norm")
 print('conv8', conv8.get_shape())
 # Now 128x384x32
 
-decoded = tf.layers.conv2d(inputs=conv8_norm, filters=3, kernel_size=(5,5), padding='same', kernel_initializer=tf.glorot_normal_initializer(), activation=None, name="decoded")
+decoded = tf.layers.conv2d(inputs=conv8_norm, filters=3, kernel_size=(3,3), padding='same', kernel_initializer=tf.glorot_normal_initializer(), activation=None, name="decoded")
 #decoded_norm = tf.layers.batch_normalization(decoded, training=is_training, name="decoded_norm")
 print('decoded', decoded.get_shape())
 # Now 128x384x3
 
-print('targets_', targets_.get_shape())
-
-loss = tf.losses.mean_squared_error(targets_, decoded)
-
-# Get cost and define the optimizer
-cost = tf.reduce_mean(loss)
 
 
-def test():
+def encode():
 
         saver = tf.train.Saver()
 
         with tf.Session() as sess:
-
                 saver.restore(sess, model_path)
                 print("Model restored.")
 
+                _encoded = sess.run(encoded)
+                print('_encoded', _encoded.shape)
+
+                for i in range(0, _encoded.shape[0]):
+                    f = './autoencoder/test/test' + str(i) + '.npz'
+                    print('_encoded[i].shape', _encoded[i].shape)
+                    np.save(f, _encoded[i])
+                    print('saved', f)
+
+def decode():
+
+        saver = tf.train.Saver()
+
+        with tf.Session() as sess:
+                saver.restore(sess, model_path)
+                print("Model restored.")
+                
+                
+                _encoded_in = sess.run(encoded_in)
+                print('_encoded_in.size', _encoded_in.shape)
+
+                _decoded = sess.run(decoded)
+
+                print('_decoded', _decoded.shape)
 
                 fig, axes = plt.subplots(nrows=1, ncols=3, sharex=True, sharey=True, figsize=(20,4))
 
-                reconstructed = sess.run(decoded)
-
                 for i in range(0,3):
-                        print(reconstructed[i])
-                        axes[i].imshow(reconstructed[i].reshape((128,384, 3)))
+                        axes[i].imshow(_decoded[i].reshape((128, 384, 3)))
                         axes[i].get_xaxis().set_visible(False)
                         axes[i].get_yaxis().set_visible(False)
 
                 fig.tight_layout(pad=0.1)
                 plt.show()
 
-
-def train():
-
-        opt = tf.train.AdamOptimizer(learning_rate).minimize(cost)
-
-        saver = tf.train.Saver()
-
-        with tf.Session() as sess:
-
-                sess.run(tf.global_variables_initializer())
-
-                for e in range(epochs):
-
-                        for ii in range(len(training_files)//batch_size):
-
-                                batch_cost, _ = sess.run([cost, opt])
-
-                                print("Epoch: {}/{}".format(e+1, epochs),
-                                "Training loss: {:.4f}".format(batch_cost), "Iteration: {}".format(ii))
-
-                        save_path = saver.save(sess, model_path)
-                        print("Model saved in path: %s" % save_path)
-
-
-train()
-
-is_training = False
-test()
+encode()
+decode()
