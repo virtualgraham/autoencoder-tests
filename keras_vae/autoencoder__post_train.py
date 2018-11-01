cd37097769420406d24a4bd33859d3184082270f
Graham Shapiro <virtualgraham@gmail.com>
2018-11-01 11:26:49 +0000

    None

---
+++ keras_vae/autoencoder__post_train.py
@@ -0,0 +1,66 @@
+from keras.models import load_model
+from keras.preprocessing import image
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import Model
+from keras.layers import Input
+
+img_width, img_height = 384, 128
+
+encoder_model = load_model('encoder_model.h5')
+decoder_model = load_model('decoder_model.h5')
+
+print(encoder_model.summary())
+print(decoder_model.summary())
+
+img_path = '/space/datasets/kitti/format1/test/2011_09_26_0079/0000000000.png'
+
+
+img = image.load_img(img_path, target_size=(img_height, img_width))
+img_tensor = image.img_to_array(img)
+img_tensor = np.expand_dims(img_tensor, axis=0)
+
+img_tensor /= 255.
+
+# Its shape is (1, 150, 150, 3)
+print(img_tensor.shape)
+
+
+
+plt.imshow(img_tensor[0])
+plt.show()
+
+autoencoder_input = Input(shape=(128, 384, 3))
+autoencoder = Model( autoencoder_input, decoder_model(encoder_model(autoencoder_input)) )
+
+pred = autoencoder.predict(img_tensor)
+
+print(pred)
+
+plt.imshow(pred[0])
+plt.show()
+
+# from keras import models
+
+# # Extracts the outputs of the top 8 layers:
+# layer_outputs = [layer.output for layer in encoder.layers[1:(len(encoder.layers)-1)]]
+# print(layer_outputs)
+# # Creates a model that will return these outputs, given the model input:
+# activation_model = models.Model(inputs=encoder.input, outputs=layer_outputs)
+
+# activations = activation_model.predict(img_tensor)
+
+
+# first_layer_activation = activations[0]
+# print(first_layer_activation.shape)
+
+# import matplotlib.pyplot as plt
+
+# plt.matshow(first_layer_activation[0, :, :, 10], cmap='viridis')
+# plt.show()
+
+# sec_layer_activation = activations[5]
+# print(sec_layer_activation.shape)
+
+# plt.matshow(sec_layer_activation[0, :, :, 29], cmap='viridis')
+# plt.show()
--- keras_vae/vae.py
+++ keras_vae/vae.py
@@ -1,7 +1,9 @@
 from keras import layers
 from keras import models
+from keras.models import Model
 from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import LambdaCallback
+from keras.layers import Input
 
 import tensorflow as tf
 sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
@@ -12,65 +14,126 @@ train_data_dir = '/space/datasets/kitti/format1/train'
 validation_data_dir = '/space/datasets/kitti/format1/test'
 nb_train_samples = 32034
 nb_validation_samples = 10112
-nb_epoch = 10
-batch_size = 13
+nb_epoch = 2
+batch_size = 10
 
 
-model = models.Sequential()
-
-# Now 128x384x3
-
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(384, 128, 3), padding='same'))
-model.add(layers.MaxPooling2D((2, 2)))
-# Now 64x192x32
-
-model.add(layers.Conv2D(48, (3, 3), activation='relu', padding='same'))
-model.add(layers.MaxPooling2D((2, 2)))
-# Now 32x96x64
-
-model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
-model.add(layers.MaxPooling2D((2, 2)))
-# Now 16x48x64
-
-model.add(layers.Conv2D(96, (3, 3), activation='relu', padding='same'))
-model.add(layers.MaxPooling2D((2, 2)))
-# Now 8x24x128
-
-model.add(layers.Conv2D(128, (3, 3), activation='relu', padding='same'))
-model.add(layers.MaxPooling2D((2, 2)))
-# Now 4x12x128
-
-  
-
-model.add(layers.UpSampling2D((2,2)))
-model.add(layers.Conv2D(128, (3, 3), activation='relu', padding='same'))
-# Now 8x24x128
-
-model.add(layers.UpSampling2D((2,2)))
-model.add(layers.Conv2D(96, (3, 3), activation='relu', padding='same'))
-# Now 16x48x64
-
-model.add(layers.UpSampling2D((2,2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
-# Now 32x96x64
-
-model.add(layers.UpSampling2D((2,2)))
-model.add(layers.Conv2D(48, (3, 3), activation='relu', padding='same'))
-# Now 64x192x32
-
-model.add(layers.UpSampling2D((2,2)))
-model.add(layers.Conv2D(32, (3, 3), activation='relu', padding='same'))
-# Now 128x384x32
-
-model.add(layers.Conv2D(3, (3, 3), activation=None, padding='same'))
-# Now 128x384x3
 
 
-print(model.summary())
+###########
+# Encoder #
+###########
 
 
-model.compile(optimizer='adadelta', loss='binary_crossentropy')
+def build_encoder():
 
+        img_input = Input(shape=(128, 384, 3))
+
+        # Now 128x384x3
+
+        enc_l1_c1 = layers.Conv2D(32, 5, activation='relu', padding='same')(img_input)
+        enc_l1_n1 = layers.BatchNormalization()(enc_l1_c1)
+        enc_l1_c2 = layers.Conv2D(32, 2, strides=2, activation='relu', padding='same')(enc_l1_n1)
+        enc_l1_n2 = layers.BatchNormalization()(enc_l1_c2)
+        # Now 64x192x32
+
+
+        enc_l2_c1 = layers.Conv2D(48, 5, activation='relu', padding='same')(enc_l1_n2)
+        enc_l2_n1 = layers.BatchNormalization()(enc_l2_c1)
+        enc_l2_c2 = layers.Conv2D(48, 2, strides=2, activation='relu', padding='same')(enc_l2_n1)
+        enc_l2_n2 = layers.BatchNormalization()(enc_l2_c2)
+        # Now 32x96x48
+
+
+        enc_l3_c1 = layers.Conv2D(64, 5, activation='relu', padding='same')(enc_l2_n2)
+        enc_l3_n1 = layers.BatchNormalization()(enc_l3_c1)
+        enc_l3_c2 = layers.Conv2D(64, 2, strides=2, activation='relu', padding='same')(enc_l3_n1)
+        enc_l3_n2 = layers.BatchNormalization()(enc_l3_c2)
+        # Now 16x48x64
+
+
+        enc_l4_c1 = layers.Conv2D(96, 5, activation='relu', padding='same')(enc_l3_n2)
+        enc_l4_n1 = layers.BatchNormalization()(enc_l4_c1)
+        # Now 16x48x96
+        enc_l4_c2 = layers.Conv2D(32, 2, strides=2, activation='relu', padding='same')(enc_l4_n1)
+        enc_l4_n2 = layers.BatchNormalization()(enc_l4_c2)
+        # Now 8x24x32
+
+        return Model(img_input, enc_l4_n2)
+
+###########
+# Decoder #
+###########
+
+def build_decoder():
+
+        enc_input = Input(shape=(8, 24, 32))
+
+        # Now 8x24x32
+
+        dec_l1_u1 = layers.UpSampling2D((2,2))(enc_input)
+        dec_l1_c1 = layers.Conv2D(96, 2, activation='relu', padding='same')(dec_l1_u1)
+        dec_l1_n1 = layers.BatchNormalization()(dec_l1_c1)
+        # Now 16x48x96
+        dec_l1_c2 = layers.Conv2D(96, 5, activation='relu', padding='same')(dec_l1_n1)
+        dec_l1_n2 = layers.BatchNormalization()(dec_l1_c2)
+        # Now 16x48x96
+
+
+        dec_l2_u1 = layers.UpSampling2D((2,2))(dec_l1_n2)
+        dec_l2_c1 = layers.Conv2D(64, 2, activation='relu', padding='same')(dec_l2_u1)
+        dec_l2_n1 = layers.BatchNormalization()(dec_l2_c1)
+        dec_l2_c2 = layers.Conv2D(64, 5, activation='relu', padding='same')(dec_l2_n1)
+        dec_l2_n2 = layers.BatchNormalization()(dec_l2_c2)
+        # Now 32x96x64
+
+
+        dec_l3_u1 = layers.UpSampling2D((2,2))(dec_l2_n2)
+        dec_l3_c1 = layers.Conv2D(48, 2, activation='relu', padding='same')(dec_l3_u1)
+        dec_l3_n1 = layers.BatchNormalization()(dec_l3_c1)
+        dec_l3_c2 = layers.Conv2D(48, 5, activation='relu', padding='same')(dec_l3_n1)
+        dec_l3_n2 = layers.BatchNormalization()(dec_l3_c2)
+        # Now 64x192x48
+
+
+        dec_l4_u1 = layers.UpSampling2D((2,2))(dec_l3_n2)
+        dec_l4_c1 = layers.Conv2D(32, 2, activation='relu', padding='same')(dec_l4_u1)
+        dec_l4_n1 = layers.BatchNormalization()(dec_l4_c1)
+        dec_l4_c2 = layers.Conv2D(32, 5, activation='relu', padding='same')(dec_l4_n1)
+        dec_l4_n2 = layers.BatchNormalization()(dec_l4_c2)
+        # Now 128x384x32
+
+
+        dec_l4_out = layers.Conv2D(3, 3, activation='relu', padding='same')(dec_l4_n2)
+        # Now 128x384x3
+
+        return Model(enc_input, dec_l4_out)
+
+###############
+# Autoencoder #
+###############
+
+encoder_model = build_encoder()
+
+print(encoder_model.summary())
+
+decoder_model = build_decoder()
+
+print(decoder_model.summary())
+
+
+autoencoder_input = Input(shape=(128, 384, 3))
+autoencoder = Model( autoencoder_input, decoder_model(encoder_model(autoencoder_input)) )
+
+print(autoencoder.summary())
+
+
+autoencoder.compile(optimizer='adam', loss='mse')
+
+def save_models():
+        print('Saving Models')
+        encoder_model.save('encoder_model.h5')
+        decoder_model.save('decoder_model.h5')
 
 def fixed_generator(generator):
     for batch in generator:
@@ -82,22 +145,23 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(
         train_data_dir,
-        target_size=(img_width, img_height),
+        target_size=(img_height, img_width),
         batch_size=batch_size,
         class_mode=None)
 
 validation_generator = test_datagen.flow_from_directory(
         validation_data_dir,
-        target_size=(img_width, img_height),
+        target_size=(img_height, img_width),
         batch_size=batch_size,
         class_mode=None)
 
-callbacks_list = [ModelCheckpoint('keras_ae.model', verbose=True)]
+callbacks_list = [LambdaCallback(on_epoch_end=lambda epoch, logs: save_models() )]
 
-history =  model.fit_generator(
+history = autoencoder.fit_generator(
         fixed_generator(train_generator),
-        samples_per_epoch=nb_train_samples,
+        steps_per_epoch=nb_train_samples//batch_size,
         nb_epoch=nb_epoch,
         callbacks=callbacks_list,
         validation_data=fixed_generator(validation_generator),
         nb_val_samples=nb_validation_samples)
+
